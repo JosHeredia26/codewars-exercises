1b845c2ed1573c9880c5e28446f74ac4196edf33
JosHeredia26 <jose.heredia.pachas@gmail.com>
2021-11-22 18:25:03 -0500

    add will you make it? exercise

--- JavaScript/StringRepeat.js
+++ JavaScript/StringRepeat.js
@@ -1,4 +1,4 @@
-//Instructions: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+// Instructions: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
 
 function repeatStr(n, s) {
   return s.repeat(n);
